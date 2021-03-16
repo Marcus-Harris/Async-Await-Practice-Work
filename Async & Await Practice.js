b7bb7f20ec4f3dfa687c6c6ff7a569a677e83c70
Marcus-Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-03-15 21:34:41 -0400

    Add files via upload

---
+++ Async & Await Practice.js
@@ -0,0 +1,27 @@
+// Write an asynchronous function and call it with async/await
+function getRandomNumber() {
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            let num = Math.floor((Math.random() * 100) + 1);
+            resolve(num)
+        }, 500);
+    });
+};
+
+
+async function callFunction() {
+    let promise = await getRandomNumber();
+    console.log(promise);
+};
+
+callFunction();
+
+// Fetch remote data using async/await
+async function getCities(cityName) {
+    const cityObject = await fetch(`https://geocode.xyz/${cityName}?json=1`)
+    const jsonConvert = await cityObject.json();
+    console.log("The latitude is: " + jsonConvert.longt +
+        "The longitude is:" + jsonConvert.latt);
+}
+
+getCities("charlotte");
