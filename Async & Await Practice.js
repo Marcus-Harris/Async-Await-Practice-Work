860c9dd721a923c6ab6eee176334af893d1d9140
Marcus-Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-03-15 22:04:58 -0400

    Update Async & Await Practice.js

--- Async & Await Practice.js
+++ Async & Await Practice.js
@@ -20,8 +20,8 @@ callFunction();
 async function getCities(cityName) {
     const cityObject = await fetch(`https://geocode.xyz/${cityName}?json=1`)
     const jsonConvert = await cityObject.json();
-    console.log("The latitude is: " + jsonConvert.latt +
-        " The longitude is: " + jsonConvert.longt);
+    console.log(`The latitude for ${cityName} is: ` + jsonConvert.latt + "." +
+        ` The longitude for ${cityName} is: ` + jsonConvert.longt + ".");
 }
 
 getCities("charlotte");
